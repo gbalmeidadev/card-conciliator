a7ade28d76b415682aa3649cd0e7a581999a9a71
Gabriel Almeida <gbjalmeida18@gmail.com>
2026-03-26 19:34:34 -0300

    fix(auth): update JwtService to use secure key and signWith API

--- src/main/java/com/conciliator/card_conciliator/Service/JwtService.java
+++ src/main/java/com/conciliator/card_conciliator/Service/JwtService.java
@@ -2,21 +2,27 @@ package com.conciliator.card_conciliator.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService{
 
-    private final String SECRET_KEY = "my-secret-key"; //depois colocar em env
+    private static final String SECRET_KEY = "12345678901234567890123456789012"; //depois colocar em env
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) //1hr
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 }
